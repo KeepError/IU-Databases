356c97d78f1983cb3275c2fa1dc00c0625f856b4
Egor Kuziakov <edkuz11@yandex.ru>
2023-03-14 17:46:07 +0300

    week 8 ex1

--- Week 8/ex1.sql
+++ Week 8/ex1.sql
@@ -1,9 +1,31 @@
-EXPLAIN
-SELECT * FROM customer
-WHERE id=756;
+-- Analyzing SELECT with no index (for results check ex1-no-index.png file)
 
-create index idx on customer(id);
-
-EXPLAIN
+EXPLAIN ANALYZE
 SELECT * FROM customer
-WHERE id=756;
+WHERE id=11756;
+
+-- Creating btree index
+
+CREATE INDEX btree_idx on customer(id);
+
+-- Analyzing SELECT with btree index (for results check ex1-btree-index.png file)
+
+EXPLAIN ANALYZE
+SELECT * FROM customer
+WHERE id=11756;
+
+-- Deleting btree index
+
+DROP INDEX btree_idx;
+
+-- Creating hash index
+
+CREATE INDEX hash_idx on customer USING hash(id);
+
+-- Analyzing SELECT with hash index (for results check ex1-hash-index.png file)
+
+EXPLAIN ANALYZE
+SELECT * FROM customer
+WHERE id=11756;
+
+DROP INDEX hash_idx;
